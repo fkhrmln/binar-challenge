7ce77bb767b4215790866d78ec438b57e094b0ca
Fakhri Maulana <fkhrmhsn110802@gmail.com>
2023-11-18 14:31:29 +0700

    feat(chapter-7): add chapter-7

--- chapter-7/index.js
+++ chapter-7/index.js
@@ -1,16 +1,22 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 const swaggerUI = require('swagger-ui-express')
-const router = require('./router')
-const swaggerDocument = require('../docs/swagger.json')
-const { MORGAN_FORMAT } = require('../config/application')
+const router = require('./app/router')
+const swaggerDocument = require('./docs/swagger.json')
+const { MORGAN_FORMAT } = require('./config/application')
+const { PORT = 5000 } = process.env
 const app = express()
 
 app.use(morgan(MORGAN_FORMAT))
-app.use(cors())
 app.use(express.json())
 app.get('/documentation.json', (req, res) => res.send(swaggerDocument))
 app.use('/documentation', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
-module.exports = router.apply(app)
+router.apply(app)
+
+
+app.listen(PORT, () => {
+  console.log("Listening on port", PORT);
+})
